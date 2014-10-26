0a4e59f6a433096eaa04f131cfa3f4c09f39ae76
Top <club-z@hotmail.com>
2014-10-26 16:24:06 +0700

    Added ProsConfig file.

---
+++ Pros/ProsConfig.swift
@@ -0,0 +1,19 @@
+//
+//  ProsConfig.swift
+//  Pros
+//
+//  Created by Suwat Saegauy on 8/17/14.
+//  Copyright (c) 2014 Posa. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Parse
+let kParseApplicationId: String     = "SqxZcUO5RiZ2nqPUaouod1YFuAPSZxt0w5PeUvOl"
+let kParseClientKey: String         = "NcjdaXy4kMCNUAGHRfulfOrP9L0PiMw724eDDD3w"
+
+// MARK: - Facebook
+let kFacebookAppId: String          = "723692027678338"
+
+// MARK: - Google Analytics
+let kGoogleAnalytics: String        = ""
