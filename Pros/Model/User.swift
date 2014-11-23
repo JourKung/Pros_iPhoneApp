31ba536e9cec6a0f57d2e61753bb9f823a508738
Top <club-z@hotmail.com>
2014-11-23 14:21:03 +0700

    Updated: 1) Xcode project files 2) Parse SDK v.1.5.0 3) Facebook SDK v.3.20

---
+++ Pros/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Pros
+//
+//  Created by Suwat Saegauy on 10/30/14.
+//  Copyright (c) 2014 com.cpekmutt.project. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+   
+}
---
+++ Pros/Utility/Utilities.swift
@@ -0,0 +1,13 @@
+//
+//  Utilities.swift
+//  Pros
+//
+//  Created by Suwat Saegauy on 11/7/14.
+//  Copyright (c) 2014 com.cpekmutt.project. All rights reserved.
+//
+
+import UIKit
+
+class Utilities: NSObject {
+   
+}
