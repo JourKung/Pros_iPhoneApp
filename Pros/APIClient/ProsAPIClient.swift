dc629385863708293b7839bbb95800042f802766
Suwat Saegauy <club-z@hotmail.com>
2015-03-29 15:33:55 +0700

    Added urlString (path) private function

--- Pros/APIClient/ProsAPIClient.swift
+++ Pros/APIClient/ProsAPIClient.swift
@@ -9,19 +9,16 @@
 import UIKit
 import Alamofire
 
-private let HTTP_METHOD_GET     = "GET"
-private let HTTP_METHOD_POST    = "POST"
-
 class ProsAPIClient: NSObject {
+    
+    private func urlString(path: String!) -> String! {
+        return (kDevBaseUrl)+(path)
+    }
    
     // ------------------------------
     // MARK: -
     // MARK: Activities
     // ------------------------------
-    
-    func test() -> Void {
-        
-    }
     
     // ------------------------------
     // MARK: -
@@ -33,15 +30,55 @@ class ProsAPIClient: NSObject {
     // MARK: Promotions
     // ------------------------------
     
+    func getPromotionsWithCompletion() -> Request {
+        let path: String! = "/list-promotions"
+        return Alamofire.request(.GET, urlString(path))
+    }
+    
+    func getPromotionDetailWithCompletion(fbId: String!, promotionId: String!) -> Request {
+        let path: String! = "/look-for-promotion/\(fbId)/\(promotionId)"
+        return Alamofire.request(.GET, urlString(path))
+    }
+    
     // ------------------------------
     // MARK: -
     // MARK: Categories
     // ------------------------------
     
+    func getCategoriesWithCompletion() -> Request {
+        let path: String! = "/list-categories"
+        return Alamofire.request(.GET, urlString(path))
+    }
+    
+    func getSubCategoriesByTypeWithCompletion(parameter: String!) -> Request {
+        let path: String! = "/list-shops-by-category/\(Utilities.cleanUrl(parameter))"
+        return Alamofire.request(.GET, urlString(path))
+    }
+    
+    func getSubCategoriesWithCompletion() -> Request {
+        let path: String! = "/list-shops"
+        return Alamofire.request(.GET, urlString(path))
+    }
+    
     // ------------------------------
     // MARK: -
     // MARK: Shop
     // ------------------------------
+    
+    func getShopByShopIdWithCompletion(parameter: String!) -> Request {
+        let path: String! = "/look-for-shop/\(parameter)"
+        return Alamofire.request(.GET, urlString(path))
+    }
+    
+    func getSubscribeByFbIdAndShopIdWithCompletion(fbId: String!, shopId: String!) -> Request {
+        let path: String! = "/is-subscribe/\(fbId)/\(shopId)"
+        return Alamofire.request(.GET, urlString(path))
+    }
+    
+    func getToggleSubscribeByFbIdAndShopIdWithCompletion(fbId: String!, shopId: String!) -> Request {
+        let path: String! = "/toggle-subscribe/\(fbId)/\(shopId)"
+        return Alamofire.request(.GET, urlString(path))
+    }
     
     // ------------------------------
     // MARK: -
