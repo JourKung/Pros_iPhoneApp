72b0274208d1f5bd03d71ba0db6aa2a841d95884
Suwat Saegauy <club-z@hotmail.com>
2015-04-04 03:14:26 +0700

    Modify User model

--- Pros/Model/User.swift
+++ Pros/Model/User.swift
@@ -15,35 +15,33 @@ class User: NSObject {
     // MARK: Properties
     // ------------------------------
     
-    var objectId: String!
-    var createdAt: NSDate!
-    var updatedAt: NSDate!
+    var objectId: String
+    var createdAt: NSDate
+    var updatedAt: NSDate
     
-    var fbId: String!
-    var name: String!
-    var gender: String!
-    var birthday: String!
-    var email: String!
-    var profileImageUrl: String!
+    var fbId: String
+    var name: String
+    var email: String
+    var profileImageUrl: String
+    var birthday: String
+    var gender: String
     
     // ------------------------------
     // MARK: -
     // MARK: Configuration
     // ------------------------------
     
-    init(objectId: String, createdAt: NSDate, updatedAt: NSDate,
-        fbId: String, name: String, gender: String, birthday: String, email: String, profileImageUrl: String) {
-            super.init()
-            
-            self.objectId = objectId
-            self.createdAt = createdAt
-            self.updatedAt = updatedAt
-            
-            self.fbId = fbId
-            self.name = name
-            self.gender = gender
-            self.birthday = birthday
-            self.email = email
-            self.profileImageUrl = profileImageUrl
+    init(objectId: String, createdAt: NSDate, updatedAt: NSDate, fbId: String, name: String, email: String, profileImageUrl: String, birthday: String, gender: String) {
+        
+        self.objectId = objectId
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        
+        self.fbId = fbId
+        self.name = name
+        self.email = email
+        self.profileImageUrl = profileImageUrl
+        self.birthday = birthday
+        self.gender = gender
     }
 }
