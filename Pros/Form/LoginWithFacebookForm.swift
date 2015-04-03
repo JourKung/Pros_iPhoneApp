c3f70eb14f59d3e584353ecae1215b73413a2b71
Suwat Saegauy <club-z@hotmail.com>
2015-04-04 03:13:54 +0700

    Added LoginWithFacebookForm class

---
+++ Pros/Form/LoginWithFacebookForm.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithFacebookForm.swift
+//  Pros
+//
+//  Created by Suwat Saegauy on 4/3/2558 BE.
+//  Copyright (c) 2558 com.cpekmutt.project. All rights reserved.
+//
+
+import UIKit
+
+class LoginWithFacebookForm: NSObject {
+    
+    var objectId: String!
+    var profile: [String: AnyObject]!
+    var accessToken: String!
+    var expirationDate: String!
+}
