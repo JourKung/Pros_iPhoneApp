6c3869eef2716337fa9f3c53a916f4f88f84704a
Suwat Saegauy <club-z@hotmail.com>
2015-04-07 02:10:20 +0700

    Modify User model and new design for parsing key-value with ObjectMapper library

--- Pros/Model/User.swift
+++ Pros/Model/User.swift
@@ -7,41 +7,51 @@
 //
 
 import UIKit
+import ObjectMapper
 
-class User: NSObject {
+//public protocol Mappable {
+//    init?(_ map: Map)
+//    mutating func mapping(map: Map)
+//}
+
+class User: Mappable {
     
     // ------------------------------
     // MARK: -
     // MARK: Properties
     // ------------------------------
     
-    var objectId: String
-    var createdAt: NSDate
-    var updatedAt: NSDate
+    var objectId: String!
+    var createdAt: NSDate!
+    var updatedAt: NSDate!
     
-    var fbId: String
-    var name: String
-    var email: String
-    var profileImageUrl: String
-    var birthday: String
-    var gender: String
+    var fbId: String?
+    var name: String?
+    var gender: String?
+    var birthday: String?
+    var email: String?
+    var profileImageUrl: String?
     
     // ------------------------------
     // MARK: -
     // MARK: Configuration
     // ------------------------------
     
-    init(objectId: String, createdAt: NSDate, updatedAt: NSDate, fbId: String, name: String, email: String, profileImageUrl: String, birthday: String, gender: String) {
+    required init?(_ map: Map) {
+        mapping(map)
+    }
+    
+    // Mappable
+    func mapping(map: Map) {
+        self.objectId <- map["objectId"]
+        self.createdAt <- (map["createdAt"], DateTransform())
+        self.updatedAt <- map["updatedAt"]
         
-        self.objectId = objectId
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-        
-        self.fbId = fbId
-        self.name = name
-        self.email = email
-        self.profileImageUrl = profileImageUrl
-        self.birthday = birthday
-        self.gender = gender
+        self.fbId <- map["id"]
+        self.name <- map["name"]
+        self.gender <- map["gender"]
+        self.birthday <- map["birthday"]
+        self.email <- map["email"]
+        self.profileImageUrl <- map[""]
     }
 }
