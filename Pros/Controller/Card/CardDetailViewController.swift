15a35eff823f28dcf0836b45e25ff7dd2992a876
Suwat Saegauy <club-z@hotmail.com>
2015-04-13 00:51:11 +0700

    Updated unwrapping after "as" for supported Xcode 6.3

--- Pros/Controller/Card/CardDetailViewController.swift
+++ Pros/Controller/Card/CardDetailViewController.swift
@@ -94,7 +94,7 @@ class CardDetailViewController: BaseTableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
         
         cell.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
         
