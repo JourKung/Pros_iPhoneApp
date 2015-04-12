1857982b999cd81af17b933ff6da11f2db4d0566
Suwat Saegauy <club-z@hotmail.com>
2015-04-13 01:05:14 +0700

    Modify Promotion model by using ObjectMapper library

--- Pros/Model/Promotion.swift
+++ Pros/Model/Promotion.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import ObjectMapper
 
-class Promotion: NSObject {
+class Promotion: Mappable {
    
     // ------------------------------
     // MARK: -
@@ -21,24 +22,46 @@ class Promotion: NSObject {
     
     var promotionID: String!
     var UserID: String!
-    var promotionPublishPoint: String!
-    var promotionPublishName: String!
-    var promotionPublishType: String!
-    var promotionPublishDescription: String!
-    var promotionPublishPoster: String!
-    var promotionPublishCreatedAt: String!
-    var promotionPublishUpdatedAt: String!
-    var promotionPublishPublishedAt: String!
-    var promotionPublishExpiredAt: String!
-    var shopName: String!
-    var shopLogoURL: String!
-    var shopType: String!
+    var promotionPublishPoint: String?
+    var promotionPublishName: String?
+    var promotionPublishType: String?
+    var promotionPublishDescription: String?
+    var promotionPublishPoster: String?
+    var promotionPublishCreatedAt: String?
+    var promotionPublishUpdatedAt: String?
+    var promotionPublishPublishedAt: String?
+    var promotionPublishExpiredAt: String?
+    var shopName: String?
+    var shopLogoURL: String?
+    var shopType: String?
 
     // ------------------------------
     // MARK: -
     // MARK: Configuration
     // ------------------------------
     
+    required init?(_ map: Map) {
+        mapping(map)
+    }
+    
+    func mapping(map: Map) {
+        self.promotionID <- map["promotionID"]
+        self.UserID <- map["UserID"]
+        self.promotionPublishPoint <- map["promotionPublishPoint"]
+        self.promotionPublishName <- map["promotionPublishName"]
+        self.promotionPublishType <- map["promotionPublishType"]
+        self.promotionPublishDescription <- map["promotionPublishDescription"]
+        self.promotionPublishPoster <- map["promotionPublishPoster"]
+        self.promotionPublishCreatedAt <- map["promotionPublishCreatedAt"]
+        self.promotionPublishUpdatedAt <- map["promotionPublishUpdatedAt"]
+        self.promotionPublishPublishedAt <- map["promotionPublishPublishedAt"]
+        self.promotionPublishExpiredAt <- map["promotionPublishExpiredAt"]
+        self.shopName <- map["shopName"]
+        self.shopLogoURL <- map["shopLogoURL"]
+        self.shopType <- map["shopType"]
+    }
+    
+    /*
     init(promotionID: String!,
         UserID: String!,
         promotionPublishPoint: String!,
@@ -56,19 +79,20 @@ class Promotion: NSObject {
             
             super.init()
             
-            self.promotionID = promotionID
-            self.UserID = UserID
-            self.promotionPublishPoint = promotionPublishPoint
-            self.promotionPublishName = promotionPublishName
-            self.promotionPublishType = promotionPublishType
-            self.promotionPublishDescription = promotionPublishDescription
-            self.promotionPublishPoster = promotionPublishPoster
-            self.promotionPublishCreatedAt = promotionPublishCreatedAt
-            self.promotionPublishUpdatedAt = promotionPublishUpdatedAt
-            self.promotionPublishPublishedAt = promotionPublishPublishedAt
-            self.promotionPublishExpiredAt = promotionPublishExpiredAt
-            self.shopName = shopName
-            self.shopLogoURL = shopLogoURL
-            self.shopType = shopType
+            self.promotionID <- promotionID
+            self.UserID <- UserID
+            self.promotionPublishPoint <- promotionPublishPoint
+            self.promotionPublishName <- promotionPublishName
+            self.promotionPublishType <- promotionPublishType
+            self.promotionPublishDescription <- promotionPublishDescription
+            self.promotionPublishPoster <- promotionPublishPoster
+            self.promotionPublishCreatedAt <- promotionPublishCreatedAt
+            self.promotionPublishUpdatedAt <- promotionPublishUpdatedAt
+            self.promotionPublishPublishedAt <- promotionPublishPublishedAt
+            self.promotionPublishExpiredAt <- promotionPublishExpiredAt
+            self.shopName <- shopName
+            self.shopLogoURL <- shopLogoURL
+            self.shopType <- shopType
     }
+    */
 }
