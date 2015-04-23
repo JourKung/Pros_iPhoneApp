4350c72dbb864246df469cfc29d6b7cb2319ad90
Suwat Saegauy <club-z@hotmail.com>
2015-04-23 09:07:12 +0700

    Updated Shop model

--- Pros/Model/Shop.swift
+++ Pros/Model/Shop.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import ObjectMapper
 
-class Shop: NSObject {
+class Shop: Mappable {
    
     // ------------------------------
     // MARK: -
@@ -19,65 +20,38 @@ class Shop: NSObject {
 //    var createdAt: NSDate!
 //    var updatedAt: NSDate!
     
-    var shopId: String!
-    var name: String!
-    var UserFirstName: String!
-    var UserLastName: String!
-    var email: String!
-    var Username: String!
-    var shopType: String!
-    var UserTermCondition: String!
-    var UserLogoImageUrl: String!
-    var UserCoverImageUrl: String!
-    var UserShopDescription: String!
-    var UserLocationLat: String!
-    var UserLocationLong: String!
-    var Telephone: String!
-    var ShopConcept: String!
-    var created_at: String!
-    var updated_at: String!
+    var shopID: String!
+    var name: String?
+    var email: String?
+    var shopType: String?
+    var description: String?
+    var address: String?
+    var latitude: String?
+    var longitude: String?
+    var tel: String?
+    var logoImageURL: String?
+    var coverImageURL: String?
     
     // ------------------------------
     // MARK: -
     // MARK: Configuration
     // ------------------------------
     
-    init(shopId: String!,
-        name: String!,
-        UserFirstName: String!,
-        UserLastName: String!,
-        email: String!,
-        Username: String!,
-        shopType: String!,
-        UserTermCondition: String!,
-        UserLogoImageUrl: String!,
-        UserCoverImageUrl: String!,
-        UserShopDescription: String!,
-        UserLocationLat: String!,
-        UserLocationLong: String!,
-        Telephone: String!,
-        ShopConcept: String!,
-        created_at: String!,
-        updated_at: String!) {
-            
-            super.init()
-            
-            self.shopId = shopId
-            self.name = name
-            self.UserFirstName = UserLastName
-            self.UserLastName = UserLastName
-            self.email = email
-            self.Username = Username
-            self.shopType = shopType
-            self.UserTermCondition = UserTermCondition
-            self.UserLogoImageUrl = UserLogoImageUrl
-            self.UserCoverImageUrl = UserCoverImageUrl
-            self.UserShopDescription = UserShopDescription
-            self.UserLocationLat = UserLocationLat
-            self.UserLocationLong = UserLocationLong
-            self.Telephone = Telephone
-            self.ShopConcept = ShopConcept
-            self.created_at = created_at
-            self.updated_at = updated_at
+    required init?(_ map: Map) {
+        mapping(map)
+    }
+    
+    func mapping(map: Map) {
+        self.shopID <- map["id"]
+        self.name <- map["name"]
+        self.email <- map["email"]
+        self.shopType <- map["shopType"]
+        self.description <- map["description"]
+        self.address <- map["address"]
+        self.latitude <- map["latitude"]
+        self.longitude <- map["longitude"]
+        self.tel <- map["tel"]
+        self.logoImageURL <- map["logoImageURL"]
+        self.coverImageURL <- map["coverImageURL"]
     }
 }
